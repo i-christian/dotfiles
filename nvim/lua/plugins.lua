d06f071a427971bcd8ce856b5f70ef9d1ed9dc43
Christian <111076892+i-christian@users.noreply.github.com>
2024-01-17 20:36:43 +0200

    my typescript, tailwindcss, react setup

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,59 @@
+local status, packer = pcall(require, "packer")
+if (not status) then
+  print("Packer is not installed")
+  return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  -- Your plugins go here
+  
+  use 'jose-elias-alvarez/null-ls.nvim'
+  use 'MunifTanjim/prettier.nvim'
+  
+  use 'L3MON4D3/LuaSnip' -- snippet
+  use 'kyazdani42/nvim-web-devicons' -- file icons
+  use {
+    'svrana/neosolarized.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' }
+  }
+
+  use {
+    'nvimdev/lspsaga.nvim',
+    after = 'nvim-lspconfig',
+    config = function()
+        require('lspsaga').setup({})
+    end,
+  } --LSP UIs
+  use 'nvim-lualine/lualine.nvim'
+  use 'onsails/lspkind-nvim' -- vscode-like pictogrmas
+  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for bufer words
+  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
+  use 'hrsh7th/nvim-cmp' -- Code completion
+  use 'neovim/nvim-lspconfig' -- LSP
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+
+  use 'windwp/nvim-ts-autotag'
+  use 'windwp/nvim-autopairs'
+
+  use 'nvim-lua/plenary.nvim' -- Common utilities
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-telescope/telescope-file-browser.nvim'
+ 
+  use 'akinsho/nvim-bufferline.lua'
+  use 'norcalli/nvim-colorizer.lua'
+
+  use 'lewis6991/gitsigns.nvim'
+  use 'dinhhuy258/git.nvim' --git blame
+
+  -- Mason-lspconfig
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+
+end)
+
