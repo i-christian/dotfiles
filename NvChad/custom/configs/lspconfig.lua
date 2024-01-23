92fbbe9894c69d956d55cd1c3ee6962f85ca72bf
Christian <111076892+i-christian@users.noreply.github.com>
2024-01-23 16:53:22 +0200

    custom settings for NvChad: Python, TS, Rust, JS, Tailwindcss language servers

---
+++ NvChad/custom/configs/lspconfig.lua
@@ -0,0 +1,67 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+local lsp_config = require("lspconfig")
+local util = require("lspconfig/util")
+
+
+-- TypeScript
+lsp_config.tsserver.setup {
+  capabilities = capabilities,
+  on_attach = on_attach,
+  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+  cmd = { "typescript-language-server", "--stdio" },
+  single_file_support = true,
+  settings = {
+    completions = {
+      completeFunctionCalls = true
+    }
+  }
+
+}
+
+--tailwindCss
+lsp_config.tailwindcss.setup{}
+
+-- Python 
+lsp_config.pyright.setup{
+  on_attach = on_attach,
+  filetypes = {"python"},
+  single_file_support = true,
+  settings = {
+    pyright = {
+      autoImportCompletion = true,
+    },
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = 'openFilesOnly',
+        useLibraryCodeForTypes = true,
+        typeCheckingMode = 'off'
+      }
+    }
+  }
+}
+
+lsp_config.ruff_lsp.setup{
+  on_attach = on_attach,
+  cmd = {"ruff-lsp"},
+  filetypes = {"python"},
+  --root_dir = see source file,
+  single_file_support = true,
+  init_options = {
+    settings = {
+      -- Any extra CLI arguments for `ruff` go here.
+      args = {},
+    }
+  }
+}
+
+-- Markdown setup
+lsp_config.marksman.setup{
+  on_attach = on_attach,
+  cmd = {"marksman", "server"},
+  filetypes = {"markdown", "markdown.mdx", "markdown.md"},
+  root_dir = util.root_pattern(".git", ".markdown.mdx"),
+  single_file_support = true
+}
+
---
+++ NvChad/custom/plugins.lua
@@ -0,0 +1,84 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "rust-analyzer",
+        "pyright",
+        "marksman",
+        "tailwindcss-language-server"
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+
+  {
+    "windwp/nvim-ts-autotag",
+    opts = {
+      autotag = {
+        enable = true,
+      }
+    },
+  },
+
+  -- Rust setup
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^3",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "mfussenegger/nvim-dap",
+      {
+        "lvimuser/lsp-inlayhints.nvim",
+        opts = {}
+      },
+    },
+    ft = { "rust" },
+    config = function()
+      vim.g.rustaceanvim = {
+        inlay_hints = {
+          highlight = "NonText",
+        },
+        tools = {
+          hover_actions = {
+            auto_focus = true,
+          },
+        },
+        server = {
+          on_attach = function(client, bufnr)
+            require("lsp-inlayhints").on_attach(client, bufnr)
+          end
+        }
+      }
+    end
+  },
+  {
+    'saecki/crates.nvim',
+    ft = {"toml"},
+    config = function(_, opts)
+      local crates  = require('crates')
+      crates.setup(opts)
+      require('cmp').setup.buffer({
+        sources = { { name = "crates" }}
+      })
+      crates.show()
+      require("core.utils").load_mappings("crates")
+    end,
+  },
+}
+
+return plugins
